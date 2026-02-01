9800b8043146f6af455dcdf2e09603442c64419f
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-02-01 11:12:23 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { useState } from 'react';
 const App = () => {
   const [name,setname] = useState('');
+  const [allUsers,setallUsers] = useState(['harsh'])
+
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("Form");
+    const newAllUsers = [...allUsers]
+    newAllUsers.push(name)
+    console.log(newAllUsers)
+    setallUsers(newAllUsers);
   }
   return (
     <div>
@@ -17,6 +22,12 @@ const App = () => {
          console.log(e.target.value)
         }}/>
         <button>Submit</button>
+        {
+          allUsers.map((elem,idx)=>{
+            return <h1 key={idx}>{elem}</h1>
+ 
+          })
+        }
       </form>
     </div>
   )
